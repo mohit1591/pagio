6b265c14cd612067b8c3f4dc78a89884f1de8551
himanshukubdee <dev1@sinelogix.com>
2026-06-02 16:26:40 +0530

    wayfinder

--- vite.config.ts
+++ vite.config.ts
@@ -21,8 +21,8 @@ export default defineConfig({
                 },
             },
         }),
-        wayfinder({
-            formVariants: true,
-        }),
+        // wayfinder({
+        //     formVariants: true,
+        // }),
     ],
 });
